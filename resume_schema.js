324d4c7897c799062be9409d948e45a8f846d286
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-03-29 20:28:22 +0300

    First deploy trial

---
+++ resume_schema.js
@@ -0,0 +1,20 @@
+module.exports = resumeSchema = {
+    "basics": {
+      "name": "",
+      "label": "",
+      "picture": "",
+      "email": "",
+      "phone": "",
+      "url": "",
+      "summary": "",
+      "location": {
+        "address": "",
+        "postalCode": "",
+        "city": "",
+        "countryCode": "",
+        "region": ""
+      },
+      "profiles": []
+    },
+
+  }
---
+++ resume_to_pdf_converter.sh
@@ -0,0 +1,1 @@
+resume export resumes/resume.pdf --theme=elegant
